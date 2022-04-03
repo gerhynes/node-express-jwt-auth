ad622999dffd96ef8404b03bb74a71e9b1d6d2ae
Gerard Hynes <gkhynes@gmail.com>
2022-04-03 15:23:32 +0100

    Add Mongoose validation

--- models/User.js
+++ models/User.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Please enter an email"],
     unique: true,
-    lowercase: true
+    lowercase: true,
+    validate: [isEmail, "Please enter a valid email"]
   },
   password: {
     type: String,
-    required: true,
-    minLength: 10
+    required: [true, "Please enter a password"],
+    minlength: [10, "Minimum password length is 10 characters"]
   }
 });
 
