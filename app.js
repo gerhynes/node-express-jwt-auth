4cbfe971d73fba12dd5ee44473872f455dad0a57
Gerard Hynes <gkhynes@gmail.com>
2022-04-04 22:28:39 +0100

    Create jwt on signup

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -20,7 +22,12 @@ mongoose
     useUnifiedTopology: true,
     useCreateIndex: true
   })
-  .then((result) => app.listen(3000))
+  .then((result) => {
+    console.log("Connected to db");
+    app.listen(3000, () => {
+      console.log("App listening on port 3000");
+    });
+  })
   .catch((err) => console.log(err));
 
 // routes
