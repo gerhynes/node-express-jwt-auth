6bc307a7e639419c61d356800b9652298108bfd6
Gerard Hynes <gkhynes@gmail.com>
2022-04-06 22:11:21 +0100

    Conditionally show user info

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -10,7 +11,6 @@ const requireAuth = (req, res, next) => {
         console.log(error.message);
         res.redirect("/login");
       } else {
-        console.log(decodedToken);
         next();
       }
     });
@@ -19,4 +19,27 @@ const requireAuth = (req, res, next) => {
   }
 };
 
-module.exports = { requireAuth };
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, process.env.SECRET, async (err, decodedToken) => {
+      if (err) {
+        console.log(error.message);
+        res.locals.user = null;
+        next();
+      } else {
+        console.log(decodedToken);
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { requireAuth, checkUser };
